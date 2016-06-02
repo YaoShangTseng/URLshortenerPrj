477e73b922e8667deeeb45577046954f0163c835
YaoShangTseng <yakushou730@gmail.com>
2016-06-02 19:27:51 +0800

    apply root page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   #root 'urlmaps#index'
 
-  #root 'urlmaps#index'
+  root 'urlmaps#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
